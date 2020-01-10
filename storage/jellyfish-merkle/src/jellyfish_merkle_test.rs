3d2eda4b1919f92d7b97527b60ad93987d030981
Zach Amsden <zamsden@calibra.com>
2020-01-10 14:53:00 -0800

    [jellyfish-merkle] Test consecutive contiguous account updates
    Testing a modified Jellyfish tree revealed some strange behavior
    in this edge case.  Adding a test to Libra to make sure we have
    coverage.

--- storage/jellyfish-merkle/src/jellyfish_merkle_test.rs
+++ storage/jellyfish-merkle/src/jellyfish_merkle_test.rs
@@ -638,6 +638,50 @@ proptest! {
     }
 
     #[test]
+    fn test_put_blob_with_proof2(
+        key1 in any::<HashValue>()
+            .prop_filter(
+                "Can't be 0xffffff...",
+                |key| *key != HashValue::new([0xff; HashValue::LENGTH]),
+            ),
+        accounts in vec(any::<AccountStateBlob>(), 2),
+    ) {
+        println!("1");
+        let db = MockTreeStore::default();
+        let tree = JellyfishMerkleTree::new(&db);
+        let mut version = 0;
+        let mut kvs = HashMap::new();
+
+        println!("1");
+        kvs.insert(key1.clone(), accounts[0].clone());
+        let (new_root_hash, batch) = tree
+            .put_blob_set(vec![(key1, accounts[0].clone())], version)
+            .unwrap();
+        db.write_tree_update_batch(batch).unwrap();
+
+        println!("2");
+        let root_hash = tree.get_root_hash(version).unwrap();
+        assert_eq!(root_hash, new_root_hash);
+        test_existent_keys_impl(&tree, 0, &kvs);
+
+        println!("3");
+        let key2 = plus_one(key1);
+        version = version + 1;
+
+        println!("4");
+        kvs.insert(key2.clone(), accounts[1].clone());
+        let (new_root_hash, batch) = tree
+            .put_blob_set(vec![(key1, accounts[1].clone())], version)
+            .unwrap();
+        db.write_tree_update_batch(batch).unwrap();
+
+        println!("5");
+        let root_hash = tree.get_root_hash(version).unwrap();
+        assert_eq!(root_hash, new_root_hash);
+        test_existent_keys_impl(&tree, 0, &kvs);
+    }
+
+    #[test]
     fn test_get_range_proof(
         (btree, n) in btree_map(any::<HashValue>(), any::<AccountStateBlob>(), 1..1000)
             .prop_flat_map(|btree| {
